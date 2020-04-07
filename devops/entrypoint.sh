0baee8de0e99ae3b5243ff482d850af87d037b5a
Mikhail Chatillon <chmikhai@microsoft.com>
2020-04-07 13:01:22 +0200

    Remove validation error

--- devops/entrypoint.sh
+++ devops/entrypoint.sh
@@ -8,7 +8,7 @@ elif [ $1 = "push" ]; then
     sudo -E iotedgedev $1 -f $2/$3 -P $4
 elif [ $1 = "deploy" ]; then
     echo "Deploying iot edge module"
-    sudo -E iotedgedev genconfig -f $2/$3 -P $4  --fail-on-validation-error true
+    sudo -E iotedgedev genconfig -f $2/$3 -P $4  --fail-on-validation-error
     sudo az extension add --name azure-iot
     sudo -E az iot edge deployment delete --login "$IOTHUB_CONNECTION_STRING" --deployment-id "$IOT_EDGE_DEPLOYMENT_ID"
     sudo -E az iot edge deployment create --login "$IOTHUB_CONNECTION_STRING" --content "config/${$3//'.template'}" --deployment-id "$IOT_EDGE_DEPLOYMENT_ID" --target-condition "deviceId='$DEVICE_ID'"
