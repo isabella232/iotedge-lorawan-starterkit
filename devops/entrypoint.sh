8978aec446a51fa27c7a55f85d1db391ff95f320
Mikhail Chatillon <chmikhai@microsoft.com>
2020-04-07 12:36:37 +0200

    Correct file path

--- devops/entrypoint.sh
+++ devops/entrypoint.sh
@@ -1,24 +1,15 @@
 #!/bin/bash
-# First argument is name of env var, second is default value.
-ensureEnvironmentVariableIsThere () {
-    if [ -z "${!1}" ]; then
-        echo "Environment Variable $1 not found, defaulting to $2"
-        export $1=$2
-    fi
-}
-
-ensureEnvironmentVariableIsThere "templateFilePath" "deployment.template.json"
 
 if [ $1 = "build" ]; then
     echo "Building iot edge module"
-    sudo -E iotedgedev $1 -f $2 -P $3
+    sudo -E iotedgedev $1 -f $2/$3 -P $4
 elif [ $1 = "push" ]; then
     echo "Pushing iot edge module"
-    sudo -E iotedgedev $1 -f $2 -P $3
+    sudo -E iotedgedev $1 -f $2/$3 -P $4
 elif [ $1 = "deploy" ]; then
     echo "Deploying iot edge module"
-    sudo -E iotedgedev genconfig -f $2 -P $3
+    sudo -E iotedgedev genconfig -f $2/$3 -P $4
     sudo az extension add --name azure-iot
     sudo -E az iot edge deployment delete --login "$IOTHUB_CONNECTION_STRING" --deployment-id "$IOT_EDGE_DEPLOYMENT_ID"
-    sudo -E az iot edge deployment create --login "$IOTHUB_CONNECTION_STRING" --content "config/deployment.json" --deployment-id "$IOT_EDGE_DEPLOYMENT_ID" --target-condition "deviceId='$DEVICE_ID'"
+    sudo -E az iot edge deployment create --login "$IOTHUB_CONNECTION_STRING" --content "config/$3" --deployment-id "$IOT_EDGE_DEPLOYMENT_ID" --target-condition "deviceId='$DEVICE_ID'"
 fi
